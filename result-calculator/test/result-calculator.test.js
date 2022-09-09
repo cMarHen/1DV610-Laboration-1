5c5c31e2e1e76e25139dc962b762ad4e685866d4
Martin Henriksson <mh225wd@student.lnu.se>
2022-09-09 16:20:50 +0200

    Add unit-tests to result-calculator

--- result-calculator/test/result-calculator.test.js
+++ result-calculator/test/result-calculator.test.js
@@ -1,14 +1,53 @@
 import { ResultCalculator } from '../ResultCalculator.js'
 
-const calculator = new ResultCalculator()
+// const numbersToUseInTest = [42, 21, 534, 23, 32, 11, 21]
+// numbersToUseInTest.map((number) => calculator.setNumberToCollection(number))
 
-const numbersToUseInTest = [42, 21, 534, 23, 32, 11, 21]
-numbersToUseInTest.map((number) => calculator.setNumberToCollection(number))
+/*
+describe('text', () => {
+  test('text', () => {
+    expect().toBe()
+  })
+})
+ */
 
-describe('read numbers', () => {
-  test('should first read all numbers in the collection', () => {
-    const numbersFoundInCollection = calculator.getAllNumbers()
+describe('ResultCalculator', () => {
+  describe('Add data to collection', () => {
+    test('Add number as string, should throw an Error', () => {
+      const calculator = new ResultCalculator()
+      expect(() => calculator.setNumberToCollection('1')).toThrow(Error)
+    })
 
-    expect(numbersFoundInCollection).toEqual(expect.arrayContaining(numbersToUseInTest))
+    test('Add string of characters, should throw an Error', () => {
+      const calculator = new ResultCalculator()
+      expect(() => calculator.setNumberToCollection('test')).toThrow(Error)
+    })
+
+    test('Add positive number, should not throw an Error', () => {
+      const calculator = new ResultCalculator()
+      expect(() => calculator.setNumberToCollection(42)).not.toThrow()
+    })
+
+    test('Add negative number, should not throw an Error', () => {
+      const calculator = new ResultCalculator()
+      expect(() => calculator.setNumberToCollection(-42)).not.toThrow()
+    })
+
+    test('Add decimal number, should not throw an Error', () => {
+      const calculator = new ResultCalculator()
+      expect(() => calculator.setNumberToCollection(42.42)).not.toThrow()
+    })
+  })
+
+  describe('Read numbers', () => {
+    const calculator = new ResultCalculator()
+
+    // Inject data to collection
+    const numbersToUseInTest = [42, 21, 534]
+    numbersToUseInTest.map((number) => calculator.setNumberToCollection(number))
+
+    test('Read all numbers from collection, should return an array', () => {
+      expect(calculator.getAllNumbers()).toEqual(numbersToUseInTest)
+    })
   })
 })
