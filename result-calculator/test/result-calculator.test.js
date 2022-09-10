4ae514aadc9ad508009c7a91011383b08a7d172c
Martin Henriksson <mh225wd@student.lnu.se>
2022-09-11 00:12:04 +0200

    Add unit tests for methods to be implemented

--- result-calculator/test/result-calculator.test.js
+++ result-calculator/test/result-calculator.test.js
@@ -35,7 +35,7 @@ describe('ResultCalculator', () => {
     const calculator = new ResultCalculator()
 
     // Inject data to collection
-    const numbersToUseInTest = [42, 21, 534, -21]
+    const numbersToUseInTest = [-21, -3, 2, 10, 15, 16, 21, 32, 42, 42, 63, 24]
     numbersToUseInTest.map((number) => calculator.setNumberToCollection(number))
 
     test('Read all numbers from collection, should return an array', () => {
@@ -43,25 +43,41 @@ describe('ResultCalculator', () => {
     })
 
     test('Highest number should be 534', () => {
-      expect(calculator.getHighestNumber()).toEqual(534)
+      expect(calculator.getHighestNumber()).toEqual(63)
     })
 
     test('Lowest number should be -21', () => {
       expect(calculator.getLowestNumber()).toEqual(-21)
     })
 
-    test('Average value should be 144', () => {
-      expect(calculator.getMeanValue()).toEqual(144)
+    test('Mean value should be 144', () => {
+      expect(calculator.getMeanValue()).toEqual(20.25)
     })
 
-    test('Mean value should be 31.5', () => {
-      expect(calculator.getMedianValue()).toEqual(31.5)
+    test('Median value should be 31.5', () => {
+      expect(calculator.getMedianValue()).toEqual(18.5)
+    })
+
+    test('Standard deviation value should be 22.6680592', () => {
+      expect(calculator.getStdDeviationValue()).toEqual(22.6680592)
+    })
+
+    test('Coefficient of variation value should be 1.11941033', () => {
+      expect(calculator.getCoefficientOfVariationValue()).toEqual(1.11941033)
     })
 
     test('Erased number collection should return an empty array when reading it.', () => {
       calculator.emptyNumberCollection()
 
       expect(calculator.getAllNumbers()).toEqual([])
+    })
+  })
+
+  describe('Calculate as static methods', () => {
+    const calculator = new ResultCalculator()
+
+    test('Calculate margin of error should return 3.0 ', () => {
+      expect(calculator.calculateMarginOfError(40, 1000)).toBe(3.0)
     })
   })
 })
